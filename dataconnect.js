151f9c64f607c4e876bf7203abd139e6fc64d996
chenjia <jchen@cheweilai.cn>
2017-08-18 14:46:27 +0800

    new

--- dataconnect.js
+++ dataconnect.js
@@ -1,5 +1,5 @@
 var mongoose=require('mongoose');
-var db=mongoose.connect('mongodb://localhost@127.0.0.1:27017/serverdata');
+mongoose.connect('mongodb://localhost@127.0.0.1:27017/serverdata');
 
 db.connection.on("error",function (error) {
     console.log('---数据库连接失败---'+error);
@@ -9,3 +9,6 @@ db.connection.on("error",function (error) {
 db.connection.on("open",function () {
     console.log('---数据库连接成功---');
 });
+
+
+module.exports=db;
